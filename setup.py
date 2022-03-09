57bef37cbdd8bc14b4cc602379549c163717fc70
Quantamentals <quantamentals101@gmail.com>
2022-03-08 20:09:29 -0500

    fixed setup

--- setup.py
+++ setup.py
@@ -1,16 +1,15 @@
-from ensurepip import version
-from gettext import install
 import setuptools 
 
 setuptools.setup(
-    name="portolioVision",
+    name="portVision",
     version="0.1",
     description="Portfolio Analysis library for Cadence practice",
     homepage="#",
     author="Michael A. Ballard",
     author_email="ballard.michael@columbia.edu",
-    packages=setuptools.find_packages(),
+    packages=['portVision','portVision.handler','portVision.portfolio'],
     install_requires=["requests","pandas","numpy","pandas-datareader","matplotlib", "scipy","yfinance","beautifulsoup4"],
+    zip_safe=False
     )
     
 
