e83e54ea17da1a000f0d6e9d6726b1efde5a314e
Quantamentals <quantamentals101@gmail.com>
2022-03-26 22:34:04 -0400

    added beta and vol

--- portVision/portfolio/risk.py
+++ portVision/portfolio/risk.py
@@ -1,7 +1,18 @@
+import numpy as np
+from scipy.stats import linregress
+from portVision.handler import datahandler
 
 def vol(port,ticker, annualized=True):
-    pass 
+    daily_vol = port[ticker].std()
+    if annualized == True:
+        return daily_vol * np.sqrt(250)
+    return daily_vol
 
+
+def beta(ticker):
+    inputs = datahandler.get_closes([ticker,'^GSPC'])
+    rets = inputs.pct_change().dropna()
+    return linregress(y=rets[ticker], x=rets['^GSPC'])[0]
 
 
 
